9ace3b0fd25c4e1cb1f0de32f10c0f18955cdafe
M-Hassan-Raza <h.raza1164@yahoo.com>
2023-04-03 11:38:54 +0500

    Addeed Components, added states, imported axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React, {useState} from 'react';
+import Form from './components/Form'
+import axios from 'axios';
 
 function App() {
+  const [searchUser, setSearchUser] = useState("");
+  const [resultUsers, setResultUsers] = useState([]);
+
   return (
     <div className="App">
-
+      <Form  />
     </div>
   );
 }
