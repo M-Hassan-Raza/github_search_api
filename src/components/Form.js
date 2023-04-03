dedf5d1a20c6ac9f516349597ef1876b3d129487
M-Hassan-Raza <h.raza1164@yahoo.com>
2023-04-03 13:42:41 +0500

    added title heading

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,6 +15,7 @@ const Form = (props) => {
     };
     return (
         <div>
+            <h1 class="form-title"></h1>
             <form id="myForm" onSubmit={handleSearch}>
                 <input type="text" placeholder='Enter any Username' value={props.searchUser} onChange={searchStateHandler}></input>
                 <button id="submit-btn" type="submit">Search</button>
